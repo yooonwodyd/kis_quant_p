82ab1eefb8b908be8deda51de02d6a49c56db734
yooonwodyd <dbswodyd00@naver.com>
2026-05-08 10:42:00 +0900

    chore: v2 Spring 프로젝트 기반 생성

---
+++ v2/src/main/java/com/kisquant/KisQuantApplication.java
@@ -0,0 +1,12 @@
+package com.kisquant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisQuantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisQuantApplication.class, args);
+	}
+}
