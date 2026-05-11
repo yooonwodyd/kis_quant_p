2ee17b6731e16944d005bc5f0c1728ae40c6f015
yooonwodyd <dbswodyd00@naver.com>
2026-05-12 01:18:00 +0900

    chore: v2 백엔드 의존성과 리소스 설정 추가

--- v2/src/main/java/com/kisquant/KisQuantApplication.java
+++ v2/src/main/java/com/kisquant/KisQuantApplication.java
@@ -2,11 +2,15 @@ package com.kisquant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class KisQuantApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KisQuantApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KisQuantApplication.class, args);
+    }
 }
